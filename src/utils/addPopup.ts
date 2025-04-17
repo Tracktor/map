a9a1984b28b8be82c518d590d5fd50b608021796
kevin <kevin.graff1@gmail.com>
2025-04-17 12:39:51 +0200

    fix: style close icon

--- src/utils/addPopup.ts
+++ src/utils/addPopup.ts
@@ -25,7 +25,7 @@ const createCloseButton = (popup: Popup): HTMLButtonElement => {
   closeButton.style.border = "none";
   closeButton.style.fontSize = "16px";
   closeButton.style.padding = "0";
-  closeButton.style.margin = "0";
+  closeButton.style.margin = "10px";
   closeButton.style.cursor = "pointer";
   closeButton.type = "button";
   closeButton.setAttribute("aria-label", "Close popup");
