7d5c7a9d23254593c056f9f4556a017711703de4
kevin <kevin.graff1@gmail.com>
2025-10-26 00:10:56 +0200

    feat: update Navbar styles for improved layout and spacing

--- example/Navbar.tsx
+++ example/Navbar.tsx
@@ -8,30 +8,28 @@ const Navbar = () => {
       sx={{
         alignItems: "center",
         backdropFilter: "blur(8px)",
-        borderRadius: 4,
+        borderRadius: 1,
         display: "flex",
         gap: 1,
-        left: 16,
+        left: 8,
         position: "fixed",
-        px: 2,
-        py: 1,
         top: 16,
         zIndex: 1000,
       }}
     >
       <Stack direction="row" spacing={1} alignItems="center">
-        <Button component={RouterLink} to="/" variant="text" color="primary" size="small">
+        <Button component={RouterLink} to="/" size="small">
           🏠 Markers
         </Button>
 
         <Divider orientation="vertical" flexItem />
-        <Button component={RouterLink} to="/route" variant="text" color="primary" size="small">
+        <Button component={RouterLink} to="/route" variant="text" size="small">
           🧭 Route
         </Button>
 
         <Divider orientation="vertical" flexItem />
 
-        <Button component={RouterLink} to="/multilines" variant="text" color="primary" size="small">
+        <Button component={RouterLink} to="/multilines" variant="text" size="small">
           🗺️ Multilines
         </Button>
       </Stack>
